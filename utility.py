7d0d3c3d2ef814dfcf51744fb0b7b3cf9c33c39f
Salim Ali <u14262208@tuks.co.za>
2023-06-01 01:17:46 +0200

    lint fixes v0 / removed unnecessary f-strings, removed trailing white spaces, added new line at end of file, removed white spaces inside dictionary

--- utility.py
+++ utility.py
@@ -4,31 +4,31 @@ def displayBoard(board):
 
     print()
     print()
-    print(f"     |     |     ")
+    print("     |     |     ")
     print(f"  {board['1']}  |  {board['2']}  |  {board['3']}  ")
-    print(f"     |     |     ")
+    print("     |     |     ")
     print(f"-----+-----+-----")
-    print(f"     |     |     ")
+    print("     |     |     ")
     print(f"  {board['4']}  |  {board['5']}  |  {board['6']}  ")
-    print(f"     |     |     ")
+    print("     |     |     ")
     print(f"-----+-----+-----")
-    print(f"     |     |     ")
+    print("     |     |     ")
     print(f"  {board['7']}  |  {board['8']}  |  {board['9']}  ")
-    print(f"     |     |     ")
+    print("     |     |     ")
     print()
     print()
 
 
 def initializeGame():
 
-    board = { "1": "", 
-              "2": "",
-              "3": "",
-              "4": "",
-              "5": "",
-              "6": "",
-              "7": "",
-              "8": "",
-              "9": "" }
+    board = {"1": "",
+             "2": "",
+             "3": "",
+             "4": "",
+             "5": "",
+             "6": "",
+             "7": "",
+             "8": "",
+             "9": ""}
 
     return board
