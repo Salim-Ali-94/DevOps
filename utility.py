453c7677fb9898facf9fd8fac086aca7005cf735
Salim Ali <u14262208@tuks.co.za>
2023-06-01 10:03:20 +0200

    getting somewhere

--- utility.py
+++ utility.py
@@ -6,35 +6,35 @@ def render_board(board):
 
     '''function to render current game board state'''
 
-    print()
-    print()
-    print("     |     |     ")
-    print(f"  {board['1']}  |  {board['2']}  |  {board['3']}  ")
-    print("     |     |     ")
-    print("-----+-----+-----")
-    print("     |     |     ")
-    print(f"  {board['4']}  |  {board['5']}  |  {board['6']}  ")
-    print("     |     |     ")
-    print("-----+-----+-----")
-    print("     |     |     ")
-    print(f"  {board['7']}  |  {board['8']}  |  {board['9']}  ")
-    print("     |     |     ")
-    print()
-    print()
+	print()
+	print()
+	print("     |     |     ")
+	print(f"  {board['1']}  |  {board['2']}  |  {board['3']}  ")
+	print("     |     |     ")
+	print("-----+-----+-----")
+	print("     |     |     ")
+	print(f"  {board['4']}  |  {board['5']}  |  {board['6']}  ")
+	print("     |     |     ")
+	print("-----+-----+-----")
+	print("     |     |     ")
+	print(f"  {board['7']}  |  {board['8']}  |  {board['9']}  ")
+	print("     |     |     ")
+	print()
+	print()
 
 
 def initialize_game():
 
     '''function to reset all game variables'''
 
-    board = {"1": "",
-             "2": "",
-             "3": "",
-             "4": "",
-             "5": "",
-             "6": "",
-             "7": "",
-             "8": "",
-             "9": ""}
+	board = {"1": "",
+			 "2": "",
+			 "3": "",
+			 "4": "",
+			 "5": "",
+			 "6": "",
+			 "7": "",
+			 "8": "",
+			 "9": ""}
 
     return board
