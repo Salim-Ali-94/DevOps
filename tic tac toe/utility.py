a8ad2c8fd4e2e2cc1bb308f93a2da5a8777b29ff
Salim Ali <u14262208@tuks.co.za>
2023-06-02 08:03:01 +0200

    implemented win state check

--- tic tac toe/utility.py
+++ tic tac toe/utility.py
@@ -1,27 +1,31 @@
 import os
 import sys
+from constants import WINNING_STATES
 
 
-openBlocks = lambda board: [key for key, value in board.items() if (value == " ")]
+# openBlocks = lambda board, player = " ": [key for key, value in board.items() if (value == player)]
+boardState = lambda board, player = " ": [key for key, value in board.items() if (value.lower().lstrip().rstrip() == player.lower().lstrip().rstrip())]
+extractStates = lambda board: { "x": boardState(board, "x"),
+								"o": boardState(board, "o") }
 
 def renderBoard(board):
 
 
 	os.system("cls")
 	print()
-	print()
-	print("     |     |     ")
-	print(f"  {board['1']}  |  {board['2']}  |  {board['3']}  ")
-	print("     |     |     ")
-	print("-----+-----+-----")
-	print("     |     |     ")
-	print(f"  {board['4']}  |  {board['5']}  |  {board['6']}  ")
-	print("     |     |     ")
-	print("-----+-----+-----")
-	print("     |     |     ")
-	print(f"  {board['7']}  |  {board['8']}  |  {board['9']}  ")
-	print("     |     |     ")
-	print()
+	# print()
+	print("\t     |     |     ")
+	print(f"\t  {board['1']}  |  {board['2']}  |  {board['3']}  ")
+	print("\t     |     |     ")
+	print("\t-----+-----+-----")
+	print("\t     |     |     ")
+	print(f"\t  {board['4']}  |  {board['5']}  |  {board['6']}  ")
+	print("\t     |     |     ")
+	print("\t-----+-----+-----")
+	print("\t     |     |     ")
+	print(f"\t  {board['7']}  |  {board['8']}  |  {board['9']}  ")
+	print("\t     |     |     ")
+	# print()
 	print()
 
 
@@ -43,18 +47,42 @@ def initializeGame():
 def promptUser(board):
 
 	renderBoard(board)
-	position = input(f"\n\nPlease select an available space: {', '.join(openBlocks(board))}\n\n")
+	position = input(f"\n\nPlease select an available space: {', '.join(boardState(board))}\n\n")
 
-	while (position.lower().lstrip().rstrip() not in openBlocks(board)):
+	while (position.lower().lstrip().rstrip() not in boardState(board)):
 
 		renderBoard(board)
-		position = input(f"\n\nPlease select an available space: {', '.join(openBlocks(board))}\n\n")
+		position = input(f"\n\nPlease select an available space: {', '.join(boardState(board))}\n\n")
 
 	return position
 
 
 def gameState(board):
 
-	if (len(openBlocks(board)) == 0):
+	if ((len(boardState(board)) == 0) or
+		(checkStates(board))):
 
 		sys.exit()
+
+
+def checkStates(board):
+
+	states = extractStates(board)
+	x = states["x"]
+	o = states["o"]
+
+	for state in WINNING_STATES:
+
+		X, O = 0, 0
+
+		for position in state:
+
+			if position in x: X += 1
+			elif position in o: O += 1
+
+			if ((X >= 3) or
+				(O >= 3)):
+
+				return True
+
+	return False
