7d20c89eed50707df3507b00591c4a9bf7887302
Salim Ali <u14262208@tuks.co.za>
2023-06-03 03:18:58 +0200

    tried editing lint config instead

--- tic tac toe/game.py
+++ tic tac toe/game.py
@@ -1,26 +1,24 @@
 import random
-# import utility
-from utility import promptUser, renderBoard, gameState, boardState, initializeGame
-# from utility import *
+import utility
 
 
 def gameManager():
 
 	while True:
 
-		human = promptUser(board)
+		human = utility.promptUser(board)
 		board[human] = "x"
-		renderBoard(board)
-		gameState(board)
-		computer = random.choice(boardState(board))
+		utility.renderBoard(board)
+		utility.gameState(board)
+		computer = random.choice(utility.boardState(board))
 		board[computer] = "o"
-		renderBoard(board)
-		gameState(board)
+		utility.renderBoard(board)
+		utility.gameState(board)
 
 
 
 
 if __name__ == "__main__":
 
-	board = initializeGame()
+	board = utility.initializeGame()
 	gameManager()
