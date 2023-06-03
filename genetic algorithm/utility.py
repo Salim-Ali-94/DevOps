ef7f7ac4bbc4ca16dd3e1da22ab46189ffb04c75
Salim Ali <u14262208@tuks.co.za>
2023-06-03 05:50:45 +0200

    created population initializer for real numbers

--- genetic algorithm/utility.py
+++ genetic algorithm/utility.py
@@ -1,5 +1,25 @@
-import numpy as np
+import random
 
-x = np.array([1, 2])
-y = np.array([3, 4])
-print(np.dot(x, y))
+
+def initializePopulation(chromosome_length, encoding = "number", domain = { "min": -100, "max": 100 }, population_size = 100):
+
+	population = generatePopulation(population_size, chromosome_length, encoding, domain)
+	return population
+
+
+def generatePopulation(population_size, chromosome_length, encoding, domain):
+
+	population = []
+
+	for i in range(population_size):
+
+		chromosome = ()
+
+		for j in range(chromosome_length):
+
+			if (encoding == "number"): gene = random.uniform(domain["min"], domain["max"])
+			chromosome = (*chromosome, gene)
+
+		population.append(chromosome)
+
+	return population
