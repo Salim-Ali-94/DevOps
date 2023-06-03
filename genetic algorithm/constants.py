de81455240d53ed599aa27b1ef4097da7f510485
Salim Ali <u14262208@tuks.co.za>
2023-06-03 09:56:40 +0200

    checking again

--- genetic algorithm/constants.py
+++ genetic algorithm/constants.py
@@ -5,4 +5,3 @@ NUMBERS = [str(number) for number in range(10)]
 ALPHABETS = list(string.ascii_lowercase) + list(string.ascii_uppercase)
 SYMBOLS = list(string.punctuation) + ["_", "\\", "%", "*", "^", ]
 CHARACTERS = ALPHABETS + NUMBERS + SYMBOLS
-WINNING_STATES = []
