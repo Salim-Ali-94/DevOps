4982e0b63ae71e2bd43b30fb8ad3cede74517d7c
Salim Ali <u14262208@tuks.co.za>
2023-06-03 09:24:11 +0200

    tried different notation

--- tic tac toe/utility.py
+++ tic tac toe/utility.py
@@ -1,6 +1,7 @@
 import os
 import sys
-from constants import WINNING_STATES
+# from constants import WINNING_STATES
+import constants
 
 
 boardState = lambda board, player = " ": [key for key, value in board.items() if (value == player)]
@@ -68,7 +69,7 @@ def checkStates(board):
 	x = states["x"]
 	o = states["o"]
 
-	for state in WINNING_STATES:
+	for state in constants.WINNING_STATES:
 
 		X, O = 0, 0
 
