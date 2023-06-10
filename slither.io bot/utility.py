c7ebd8a6820ca84bec50038005fe1a003025fc82
Salim Ali <u14262208@tuks.co.za>
2023-06-10 16:23:23 +0200

    bare bones puppeteer automation docker containerization

---
+++ slither.io bot/utility.py
@@ -0,0 +1,16 @@
+import asyncio
+import time
+from pyppeteer import launch
+
+
+async def main():
+
+	browser = await launch({ "headless": True, "args": ["--no-sandbox"] })
+	url = await browser.newPage()
+	await url.goto("https://scrapeme.live/shop/")
+	await browser.close()
+
+
+
+
+asyncio.get_event_loop().run_until_complete(main())
