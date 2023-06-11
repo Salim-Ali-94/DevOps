3a8002a4a1b0f2d4a0a230ec40ccd69fc126d45b
Salim Ali <u14262208@tuks.co.za>
2023-06-11 14:00:40 +0200

    modularized code and added bot as a build case for the workflow

--- slither.io bot/utility.py
+++ slither.io bot/utility.py
@@ -2,40 +2,56 @@ import asyncio
 from pyppeteer import launch
 
 
-async def bot():
+async def bot(name):
 
 	url = "https://slither.io/"
-	browser = await launch({ "headless": False,
-							 "autoClose": False,
+	browser = await launch({ "headless": True,
+							 "autoClose": True,
 							 "args": [ "--start-maximized",
 							 		   "--window-size=1920,1080" ] })
 
 	page = await browser.newPage()
-	await page.setViewport({ "width": 1920, 
+	await page.setViewport({ "width": 1920,
 							 "height": 1080 })
 
 	await page.goto(url)
 	name_input = await page.querySelector("#nick")
-	await name_input.type("snake_name")
+	await name_input.type(name)
 	await name_input.press("Enter")
-	await asyncio.sleep(2)
-	await page.keyboard.down("ArrowLeft")
-	await asyncio.sleep(2)
-	await page.keyboard.up("ArrowLeft")
 	await asyncio.sleep(1)
-	await page.keyboard.down("ArrowRight")
-	await asyncio.sleep(2)
-	await page.keyboard.up("ArrowRight")
+	page = await pressKey(page, "ArrowLeft", 1)
 	await asyncio.sleep(1)
-	await page.keyboard.down("ArrowUp")
-	await asyncio.sleep(5)
-	await page.keyboard.up("ArrowUp")
+	page = await checkReplay(page)
+	page = await pressKey(page, "ArrowRight", 1)
+	await asyncio.sleep(1)
+	page = await checkReplay(page)
+	page = await pressKey(page, "ArrowUp", 1)
 	await asyncio.sleep(1)
 	await browser.close()
 
 
+async def pressKey(page, key, delay):
+
+	await page.keyboard.down(key)
+	await asyncio.sleep(delay)
+	await page.keyboard.up(key)
+	return page
 
 
-if __name__ == "__main__":
+async def checkReplay(page):
 
-	asyncio.run(bot())
+	xpath = "//div[contains(text(), 'Play')]"
+
+	try:
+
+		button = await page.xpath(xpath)
+
+		if button:
+
+			await button[0].click()
+
+	except Exception as error:
+
+		pass
+
+	return page
