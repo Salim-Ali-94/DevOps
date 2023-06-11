255ca9535bff85b0ed030da4fcd71e914f2a40c5
Salim Ali <u14262208@tuks.co.za>
2023-06-11 13:23:58 +0200

    added some arrow click actions to bot

--- slither.io bot/utility.py
+++ slither.io bot/utility.py
@@ -2,14 +2,39 @@ import asyncio
 from pyppeteer import launch
 
 
-async def main():
+async def bot():
 
-	browser = await launch({ "headless": True, "args": ["--no-sandbox"] })
-	url = await browser.newPage()
-	await url.goto("https://scrapeme.live/shop/")
-	await browser.close()
+	url = "https://slither.io/"
+	browser = await launch({ "headless": False,
+							 "autoClose": False,
+							 "args": [ "--start-maximized",
+							 		   "--window-size=1920,1080" ] })
+
+	page = await browser.newPage()
+	await page.setViewport({ "width": 1920, 
+							 "height": 1080 })
+
+	await page.goto(url)
+	name_input = await page.querySelector("#nick")
+	await name_input.type("snake_name")
+	await name_input.press("Enter")
+	await asyncio.sleep(2)
+	await page.keyboard.down("ArrowLeft")
+	await asyncio.sleep(2)
+	await page.keyboard.up("ArrowLeft")
+	await asyncio.sleep(1)
+	await page.keyboard.down("ArrowRight")
+	await asyncio.sleep(2)
+	await page.keyboard.up("ArrowRight")
+	await asyncio.sleep(1)
+	await page.keyboard.down("ArrowUp")
+	await asyncio.sleep(5)
+	await page.keyboard.up("ArrowUp")
+	await asyncio.sleep(1)
 
 
 
 
-asyncio.get_event_loop().run_until_complete(main())
+if __name__ == "__main__":
+
+	asyncio.run(bot())
