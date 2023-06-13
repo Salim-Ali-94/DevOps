7b5fa397088fa57c06745e580ba7d246de014a0e
Salim Ali <u14262208@tuks.co.za>
2023-06-13 06:04:47 +0200

    wrote and tested hex to decimal converter

--- go projects/base-n converter/main.go
+++ go projects/base-n converter/main.go
@@ -5,8 +5,12 @@ import "fmt"
 func main() {
 
 	value := "111001"
-	fmt.Println(convertToDecimal(value))
+	fmt.Println(binaryToDecimal(value))
 	number := 57
-	fmt.Println(convertToBinary(number))
+	fmt.Println(decimalToBinary(number))
+	hex := "57"
+	fmt.Println(hexToDecimal(hex))
+	hex = "0x57"
+	fmt.Println(hexToDecimal(hex))
 
 }
--- go projects/base-n converter/utility.go
+++ go projects/base-n converter/utility.go
@@ -4,24 +4,24 @@ import ( "fmt"
 		 "strconv" )
 
 
-func convertToDecimal(number string) string {
+func binaryToDecimal(number string) string {
 
 	flag := binaryTest(number)
 
 	if flag {
 
-		accumulate := 0.0
+		accumulator := 0.0
 
 		for index, binary := range number {
 
 			digit := string(binary)
 			bit, _ := strconv.ParseFloat(digit, 64)
 			exponent := float64(len(number) - index)
-			accumulate += bit*math.Pow(2, exponent - 1)
+			accumulator += bit*math.Pow(2, exponent - 1)
 
 		}
 
-		decimal := strconv.FormatFloat(accumulate, 'f', -1, 64)
+		decimal := strconv.FormatFloat(accumulator, 'f', -1, 64)
 		return decimal
 
 	}
@@ -30,7 +30,7 @@ func convertToDecimal(number string) string {
 
 }
 
-func convertToBinary(number int) string {
+func decimalToBinary(number int) string {
 
 	decimal := float64(number)
 	length := math.Log2(decimal)
@@ -61,11 +61,47 @@ func convertToBinary(number int) string {
 
 }
 
+func hexToDecimal(number string) string {
+
+	if ((number[:2] == "0x") ||
+		(number[:2] == "0X")) {
+
+		flag := hexTest(number[2:])
+
+		if flag {
+
+			accumulator := 0.0
+
+			for index, digit := range number[2:] {
+
+				key := string(digit)
+				value := format[key]
+				hex, _ := strconv.ParseFloat(value, 64)
+				exponent := float64(len(number[2:]) - index)
+				accumulator += hex*math.Pow(16, exponent - 1)
+
+			}
+
+			decimal := strconv.FormatFloat(accumulator, 'f', -1, 64)
+			return decimal
+
+		}
+
+		return fmt.Sprintf("%q is not a hex value", number)
+
+	}
+
+	return fmt.Sprintf("%q is not a hex value", number)
+
+}
+
+
 func binaryTest(number string) bool {
 
 	for _, value := range number {
 
-		if ((string(value) != "0") && (string(value) != "1")) {
+		if ((string(value) != "0") &&
+			(string(value) != "1")) {
 
 			return false
 
@@ -74,5 +110,53 @@ func binaryTest(number string) bool {
 	}
 
 	return true
+
+}
+
+func hexTest(number string) bool {
+
+	valid := true
+
+	for _, value := range number {
+
+		valid = false
+		allowed := array()
+
+		for _, key := range allowed {
+
+
+			if (string(value) == string(key)) {
+
+				valid = true
+				break
+			
+			}
+
+		}
+
+		if !valid {
+
+			return valid
+
+		}
+
+	}
+
+	return valid
+
+}
+
+func array() []string {
+
+	keys := make([]string, 0, 16)
+
+	for index := 0; index < 16; index++ {
+
+		value := strconv.FormatInt(int64(index), 10)
+		keys = append(keys, format[value])
+
+	}
+
+	return keys
 
 }
