b522660c6a092fcdb3a616922518246ccec1216d
Salim Ali <u14262208@tuks.co.za>
2023-06-18 17:58:58 +0200

    created feed forward function for propagating the input data to the output

--- genetic algorithm/neat/utility.py
+++ genetic algorithm/neat/utility.py
@@ -26,3 +26,40 @@ def neuralNetwork(architecture):
 		network.append(weights)
 
 	return network
+
+def feedForward(data, network, architecture):
+
+	activity = data.copy()
+
+	for layer, neuron in enumerate(network):
+
+		flag = architecture[layer + 1]["bias"]
+
+		if flag:
+
+			activity = np.vstack((activity, 1))
+
+		output = neuron.dot(activity)
+		activity = activation(output, architecture[layer + 1]["function"])
+
+	return activity
+
+def activation(data, function = "sigmoid"):
+
+	if (function == "sigmoid"):
+
+		return 1 / (1 + np.exp(-data))
+
+	elif (function == "tanh"):
+
+		return np.tanh(data)
+
+	elif (function == "relu"):
+
+		return np.maximum(data, 0)
+
+	elif (function == "step"):
+
+		return np.sign(data)
+
+	return data
