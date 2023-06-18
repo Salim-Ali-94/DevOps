70cd4e101411a15f4976accc949c550cd076971d
Salim Ali <u14262208@tuks.co.za>
2023-06-18 19:37:40 +0200

    moved neat functions to the genetic algorithm utility file

--- genetic algorithm/utility.py
+++ genetic algorithm/utility.py
@@ -1,5 +1,6 @@
 import random
 import os
+import numpy as np
 from constants import CHARACTERS
 
 
@@ -355,6 +356,72 @@ def generatePopulation(chromosome_length = 2,
 		population.append(chromosome)
 
 	return population
+
+
+def neuralNetwork(architecture):
+
+	network = []
+
+	for layer in range(len(architecture) - 1):
+
+		flag = architecture[layer + 1]["bias"]
+		forward = architecture[layer + 1]["nodes"]
+
+		if (layer == 0):
+
+			current = architecture[layer]
+
+		else:
+
+			current = architecture[layer]["nodes"]
+
+		if flag:
+
+			current += 1
+
+		weights = np.random.rand(forward, current)
+		network.append(weights)
+
+	return network
+
+
+def feedForward(data, network, architecture):
+
+	activity = data.copy()
+
+	for layer, neuron in enumerate(network):
+
+		flag = architecture[layer + 1]["bias"]
+
+		if flag:
+
+			activity = np.vstack((activity, 1))
+
+		output = neuron.dot(activity)
+		activity = activation(output, architecture[layer + 1]["function"])
+
+	return activity
+
+
+def activation(data, function = "sigmoid"):
+
+	if (function == "sigmoid"):
+
+		return 1 / (1 + np.exp(-data))
+
+	elif (function == "tanh"):
+
+		return np.tanh(data)
+
+	elif (function == "relu"):
+
+		return np.maximum(data, 0)
+
+	elif (function == "step"):
+
+		return np.sign(data)
+
+	return data
 
 
 def wordScore(chromosome, target):
