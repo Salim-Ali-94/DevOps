c0854bc03a0e233ae596fc408effc2b7651587b2
Salim Ali <u14262208@tuks.co.za>
2023-06-18 12:04:54 +0200

    removed all next.js boilerplate code

--- kubernetes/todo list/ui/pages/_app.js
+++ kubernetes/todo list/ui/pages/_app.js
@@ -1,5 +1,14 @@
-import '@/styles/globals.css'
+import "@/styles/globals.css";
+import { Lato } from "next/font/google";
+
+
+const lato = Lato({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    
+    <main className={lato.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
