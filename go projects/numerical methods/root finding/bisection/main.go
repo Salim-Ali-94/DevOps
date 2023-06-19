a3a94fe049ecce3634a165064a8c311cec4704f6
Salim Ali <u14262208@tuks.co.za>
2023-06-19 04:48:54 +0200

    created function that partitions window into sections where roors exist

---
+++ go projects/numerical methods/root finding/bisection/main.go
@@ -0,0 +1,16 @@
+package main
+import ( "fmt"
+		 "math" )
+
+
+func main() {
+
+	f := func(x float64) float64 {
+
+		return math.Pow(x, 3) - math.Pow(x, 2) - 2*x + 1
+	}
+
+	d := [2]float64{-2, 2}
+	brackets := findInterval(f, d)
+
+}
