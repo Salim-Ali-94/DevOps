0d7ec739bde7e8a37ce4f27de4f9e23868bca626
Salim Ali <u14262208@tuks.co.za>
2023-07-03 06:02:03 +0200

    found all roots for an example function

--- go projects/numerical methods/root finding/bisection/utility.go
+++ go projects/numerical methods/root finding/bisection/utility.go
@@ -1,4 +1,5 @@
 package main
+import "math"
 
 
 func findInterval(function func(float64) float64, window [2]float64) []map[string]float64 {
@@ -44,8 +45,58 @@ func findInterval(function func(float64) float64, window [2]float64) []map[strin
 }
 
 
-// func bisection(function func(float64) float64, interval []map[string]float64, precision float64) []map[string]float64 {
+func bisection(function func(float64) float64, interval []map[string]float64, precision float64) ([]float64, int) {
 
+	var roots []float64
+	var left float64
+	var right float64
+	var center float64
+	var residual float64
+	var previous float64
+	iteration := 0
 
+	for _, bracket := range interval {
 
-// }
+		left = bracket["lower"]
+		right = bracket["upper"]
+		center = (left + right) / 2
+		residual = 2.0*precision
+		previous = center
+
+		for (residual > precision) {
+
+			if ((function(left)*function(center) == 0) ||
+				(function(right)*function(center) == 0)) {
+
+				roots = append(roots, center)
+				iteration += 1
+				break
+
+			} else if (function(left)*function(center) < 0) {
+
+				right = center
+
+			} else {
+
+				left = center
+
+			}
+
+			center = (left + right) / 2
+			residual = math.Abs((center - previous) / center)
+			previous = center
+			iteration += 1
+
+			if (residual < precision) {
+
+				roots = append(roots, center)
+
+			}
+
+		}
+
+	}
+
+	return roots, iteration + 1
+
+}
