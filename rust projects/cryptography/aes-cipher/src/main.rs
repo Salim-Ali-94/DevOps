e4122366d40cfcb61fcf016b3b0e760382ebfdd9
Salim Ali <u14262208@tuks.co.za>
2023-07-12 05:51:18 +0200

    used rng module and created function that returns multiple values

--- rust projects/cryptography/aes-cipher/src/main.rs
+++ rust projects/cryptography/aes-cipher/src/main.rs
@@ -3,6 +3,9 @@ mod utility;
 
 fn main() {
 
-    utility::aesKeyGenerator();
+    utility::aesKeyGenerator(-1);
+    let (x, y) = utility::aesKeyGenerator(999);
+    println!("key length is = {}", x);
+    println!("number of rounds = {}", y);
 
 }
--- rust projects/cryptography/aes-cipher/src/utility.rs
+++ rust projects/cryptography/aes-cipher/src/utility.rs
@@ -2,10 +2,32 @@
 use rand::Rng;
 
 
-pub fn aesKeyGenerator() {
+pub fn aesKeyGenerator(mut key_length: i32) -> (String, i32) {
+
+	let mut rounds = 0;
+
+	if key_length <= 128 {
+
+		key_length = 128;
+		rounds = 10;
+
+	} else if key_length > 128 && key_length <= 192 {
+
+		key_length = 192;
+		rounds = 12;
+
+	} else if key_length >= 256 {
+
+		key_length = 256;
+		rounds = 14;
+
+	}
 
 	let mut rng = rand::thread_rng();
 	let x = rng.gen_range(1..=100);
 	println!("aes key: {}", x);
+	println!("aes key length: {}", key_length);
+	println!("aes number of rounds: {}", rounds);
+	return (key_length.to_string(), rounds);
 
 }
