6dd69713555e7c8a814cfdafca0005e39b5a3d7d
Salim Ali <u14262208@tuks.co.za>
2023-07-12 18:59:11 +0200

    removed warnings and created a bit mixing function

--- rust projects/cryptography/aes-cipher/src/main.rs
+++ rust projects/cryptography/aes-cipher/src/main.rs
@@ -6,7 +6,7 @@ fn main() {
     utility::aesKeyGenerator(-1);
     utility::aesKeyGenerator(150);
     let (x, y) = utility::aesKeyGenerator(999);
-    println!("key length is = {}", x);
+    println!("the aes key is = {}", x);
     println!("number of rounds = {}", y);
 
 }
--- rust projects/cryptography/aes-cipher/src/utility.rs
+++ rust projects/cryptography/aes-cipher/src/utility.rs
@@ -1,10 +1,14 @@
 #![allow(non_snake_case)]
 use rand::Rng;
+use num_bigint::BigUint;
+use std::str::FromStr;
 
 
-pub fn aesKeyGenerator(mut key_length: i32) -> (String, i32) {
+pub fn aesKeyGenerator(mut key_length: i32) -> (BigUint, i32) {
 
-	let mut rounds = 0;
+	let mut rng = rand::thread_rng();
+	let mut key = if rng.gen_range(0.0..1.0) < 0.5 { "1".to_owned() } else { "0".to_owned() };
+	let rounds;
 
 	if key_length <= 128 {
 
@@ -16,18 +20,59 @@ pub fn aesKeyGenerator(mut key_length: i32) -> (String, i32) {
 		key_length = 192;
 		rounds = 12;
 
-	} else if key_length > 192 {
+	} else {
 
 		key_length = 256;
 		rounds = 14;
 
 	}
 
+	while key.len() < key_length as usize {
+
+        if rng.gen_range(0.0..1.0) < 0.5 {
+
+            key.push_str("1");
+
+        } else {
+
+            key.push_str("0");
+
+        }
+        
+	}
+
+	return (BigUint::from_str(&shuffleBits(key)).unwrap(), rounds);
+
+}
+
+fn shuffleBits(bits: String) -> String {
+
 	let mut rng = rand::thread_rng();
-	let x = rng.gen_range(1..=100);
-	println!("aes key: {}", x);
-	println!("aes key length: {}", key_length);
-	println!("aes number of rounds: {}", rounds);
-	return (key_length.to_string(), rounds);
+	let mut word = String::from(&bits);
+	let mut address_a;
+	let mut address_b;
+	let mut bit_a;
+	let mut bit_b;
+
+	for _ in 0..bits.len() {
+
+		address_a = rng.gen_range(0..bits.len());
+		bit_a = word.chars().nth(address_a).unwrap();
+		address_b = rng.gen_range(0..bits.len());
+		bit_b = word.chars().nth(address_b).unwrap();
+
+		while bit_b == bit_a || address_b == address_a {
+
+			address_b = rng.gen_range(0..bits.len());
+			bit_b = word.chars().nth(address_b).unwrap();
+
+		}
+
+		word.replace_range(address_a..address_a + 1, &bit_b.to_string());
+		word.replace_range(address_b..address_b + 1, &bit_a.to_string());
+
+	}
+
+	return word;
 
 }
