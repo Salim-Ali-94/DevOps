7fbe0af8b090610ecf4997e205e50185f85768b1
Salim Ali <u14262208@tuks.co.za>
2023-07-18 07:18:55 +0200

    some formatting

--- rust projects/cryptography/aes-cipher/src/main.rs
+++ rust projects/cryptography/aes-cipher/src/main.rs
@@ -8,9 +8,9 @@ fn main() {
     let (encrypted_msg, key, encrypted_doc, hex_key) = utility::scrambleDocument(msg.clone(), aes_standard);
     println!("\n\nthe aes key is = {} / {}-bits\n\n", key, key.len());
     println!("stacked hex key is = {:?}\n\n", hex_key);
-    println!("original message = '{}'\n\n", msg.clone());
+    println!("original message is = '{}'\n\n", msg.clone());
     println!("for {}-bit aes standard:\n", aes_standard);
-    println!("encrypted message = {:?}\n\n", encrypted_msg);
+    println!("encrypted message is = {:?}\n\n", encrypted_msg);
     println!("encrypted message matrix = {:?}\n\n", encrypted_doc);
 
 }
