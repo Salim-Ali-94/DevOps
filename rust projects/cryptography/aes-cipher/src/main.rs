536182a648a7fbd3b91f151471db92398e18a3f0
Salim Ali <u14262208@tuks.co.za>
2023-08-13 11:28:57 +0200

    implemented aes decrytion

--- rust projects/cryptography/aes-cipher/src/main.rs
+++ rust projects/cryptography/aes-cipher/src/main.rs
@@ -3,14 +3,17 @@ mod utility;
 
 fn main() {
 
-    let msg = "Lift off".to_string();
-    let aes_standard = 192;
-    let (encrypted_msg, key, encrypted_doc, hex_key) = utility::scrambleDocument(msg.clone(), aes_standard);
+    // let msg = "Lift off".to_string();
+    let msg = "Let none ignorant of calculus enter here.".to_string();
+    // let aes_standard = 128;
+    // let aes_standard = 192;
+    let aes_standard = 256;
+    let (encrypted_msg, key) = utility::scrambleDocument(msg.clone(), aes_standard);
     println!("\n\nthe aes key is = {} / {}-bits\n\n", key, key.len());
-    println!("stacked hex key is = {:?}\n\n", hex_key);
     println!("original message is = '{}'\n\n", msg.clone());
     println!("for {}-bit aes standard:\n", aes_standard);
-    println!("encrypted message is = {:?}\n\n", encrypted_msg);
-    println!("encrypted message matrix = {:?}\n\n", encrypted_doc);
+    println!("encrypted message is = {}\n\n", encrypted_msg);
+    let text = utility::extractDocument(encrypted_msg, key);
+    println!("message = {}\n\n", text);
 
 }
